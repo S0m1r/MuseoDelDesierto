398077d14e605f30ea693c0e97d14fa956267ea0
S0m1r <sotonana65@gmail.com>
2025-01-14 00:39:19 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,688 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pabellni1",
+      "name": "PabellónI(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19524463866732944,
+          "pitch": 0.07083820080624648,
+          "rotation": 0,
+          "target": "1-pabellni2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pabellni2",
+      "name": "PabellónI(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23583802886965088,
+          "pitch": 0.001071701695039451,
+          "rotation": 0,
+          "target": "2-pabellnii1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pabellnii1",
+      "name": "PabellónII(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20120019718921967,
+          "pitch": 0.004691463438479815,
+          "rotation": 0,
+          "target": "3-pabellnii2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pabellnii2",
+      "name": "PabellónII(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.01230787584203874,
+        "pitch": 0.2274450746598582,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19577969278167728,
+          "pitch": -0.08322491651775543,
+          "rotation": 0,
+          "target": "4-pabellniii1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pabellniii1",
+      "name": "PabellónIII(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.021557127490536843,
+          "pitch": 0.08514029475409579,
+          "rotation": 0,
+          "target": "5-pabellniii2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pabellniii2",
+      "name": "PabellónIII(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.0865090103185278,
+        "pitch": 0.12111506275329731,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6300456599390394,
+          "pitch": -0.030483507023525647,
+          "rotation": 0,
+          "target": "6-pabellniii3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pabellniii3",
+      "name": "PabellónIII(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7511018483623211,
+        "pitch": -0.15057282202140065,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03523059351716462,
+          "pitch": -0.18351106583216747,
+          "rotation": 0,
+          "target": "7-pabellniii4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pabellniii4",
+      "name": "PabellónIII(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6639778606229783,
+        "pitch": 0.09927306982732098,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2048983903967176,
+          "pitch": 0.06447366977577751,
+          "rotation": 0,
+          "target": "8-pabellniv1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pabellniv1",
+      "name": "PabellónIV(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9900560194531511,
+        "pitch": 0.07458933937359724,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6148641438965488,
+          "pitch": 0.07583096959984914,
+          "rotation": 0,
+          "target": "9-pabellniv2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pabellniv2",
+      "name": "PabellónIV(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.325216744242379,
+        "pitch": 0.16060454852049588,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2693054545489808,
+          "pitch": 0.1775481518089599,
+          "rotation": 0,
+          "target": "10-pabellniv3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pabellniv3",
+      "name": "PabellónIV(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1133263048692612,
+        "pitch": 0.17481787707609264,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2777543411037708,
+          "pitch": 0.10802016277367343,
+          "rotation": 0,
+          "target": "11-pabellniv4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pabellniv4",
+      "name": "PabellónIV(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.1611225144177908,
+        "pitch": 0.20927735982669304,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1473444692020358,
+          "pitch": 0.10632922303892656,
+          "rotation": 0,
+          "target": "12-pabellniv5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pabellniv5",
+      "name": "PabellónIV(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.2438749323046743,
+        "pitch": 0.10846950026766322,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3231079042808798,
+          "pitch": 0.07724615142869062,
+          "rotation": 0,
+          "target": "13-pabellniv6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pabellniv6",
+      "name": "PabellónIV(6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.2541180342573064,
+        "pitch": 0.035471796330819316,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24034476084329448,
+          "pitch": 0.018254540654638163,
+          "rotation": 0,
+          "target": "14-albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas1",
+      "name": "Albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.9654111787568578,
+        "pitch": 0.10920296528521334,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.327749117741428,
+          "pitch": -0.019551328805954782,
+          "rotation": 0,
+          "target": "15-albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas2",
+      "name": "Albergue_de_perrito_de_la_padrera_zorrillos_y_tortugas(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.077807437183946,
+        "pitch": 0.07807331341036772,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2943453470916495,
+          "pitch": 0.01579312907211161,
+          "rotation": 0,
+          "target": "16-jardn_botnico"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-jardn_botnico",
+      "name": "Jardín_Botánico",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0698362140633613,
+        "pitch": 0.16880278689350448,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0935055011664385,
+          "pitch": 0.0789437018528183,
+          "rotation": 0,
+          "target": "17-tienda_cactus"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-tienda_cactus",
+      "name": "Tienda_Cactus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.0453417111976417,
+        "pitch": 0.03702930708406704,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
